b5a393b7d910381bb1b47d2897eb0095d7054a96
Tallie Elisha <tallieyelisha@gmail.com>
2018-07-25 18:42:55 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,15 @@
-#write out your code here
-
 def least_coins(cents)
+  centsLeft = cents
+  quarters = cents/25
+  centsLeft =centsLeft%25 
+  dimes = centsLeft/10
+  centsLeft = centsLeft%10
+  nickels = centsLeft/5 
+  centsLeft = centsLeft%5
+  pennies = centsLeft
+   leastCoins = {"quarters"=> quarters, "dimes" => dimes, "nickels" => nickels, "pennies" => pennies}
+  puts leastCoins
+  end 
 
-#Code your answer here!
-
-end
+least_coins(14)
+least_coins(237)
